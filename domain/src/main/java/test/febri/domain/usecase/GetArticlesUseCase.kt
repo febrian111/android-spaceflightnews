11ee51d4b47972a0931665a888d53b1d743f2832
Febriansyah <febrian411@gmail.com>
2025-03-05 16:05:30 +0700

    [update] setup articles usecase

--- domain/src/main/java/test/febri/domain/usecase/GetArticlesUseCase.kt
+++ domain/src/main/java/test/febri/domain/usecase/GetArticlesUseCase.kt
@@ -1,20 +1,19 @@
 package test.febri.domain.usecase
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import test.febri.domain.AppDataRepository
 import test.febri.domain.model.ArticleModel
+import test.febri.domain.util.AppConst.SortOrder
 import javax.inject.Inject
 
 class GetArticlesUseCase @Inject constructor(
     private val repository: AppDataRepository
 ) {
-    operator fun invoke(): Flow<PagingData<ArticleModel>> {
-        return Pager(
-            config = PagingConfig(pageSize = 30),
-            pagingSourceFactory = { repository.getArticlesPagingSource(,) }
-        ).flow
-    }
+    operator fun invoke(
+        titleQuery: String = "",
+        newsSite: String = "",
+        sortOrder: SortOrder = SortOrder.SORT_ASC_PUBLISH_DATE
+    ): Flow<PagingData<ArticleModel>> =
+        repository.getArticlesPagingSource(titleQuery, newsSite, sortOrder)
 }
